56dc63950208ee9fc4d21c20a942ab601e17e17e
thisClemons <74882592+thisClemons@users.noreply.github.com>
2021-03-17 17:00:43 -0400

    removed unused functionality (@@all)

--- lib/news_reader/article.rb
+++ lib/news_reader/article.rb
@@ -2,35 +2,19 @@ class NewsReader::Article
     attr_accessor :url, :title, :source, :author, :date, :mins, :body, :section, 
     :downloaded
 
-    # @@downloaded = []
-    # @@all = []
-
     def initialize(section, title_url_hash)
         @section = section
         @title = title_url_hash[:title]
         @url = title_url_hash[:url]
         @downloaded = false 
         
-        # @@all << self
         section.add_article(self)
     end
 
-    # def self.all
-    #     @@all        
-    # end
 
-    def self.all=(sorted)
-        @@all = sorted        
-    end
-
-    # def self.downloaded
-    #     @@downloaded
-    # end
-    
     def fetch_content
         if !body
             NewsReader::Scraper.scrape_article(self)
-            # @@downloaded << self
         end
     end
 
